416287f31244cf75ca572eb6e45498124f7ed594
Md. Jubayer Ahmad <mdjubayerahmad16@gmail.com>
2024-07-02 13:13:38 +0600

    Add files via upload

---
+++ multiplication_table.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+     int number;
+     cin >> number;
+
+     for (int i = 1; i <=12; i++)
+     {
+         cout <<number << " * " << i << " = " << (number*i) <<endl;
+     }
+
+return 0;
+}
