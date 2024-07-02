3f11112618e7ea56f0a2f2f9f0364c5c33890704
Md. Jubayer Ahmad <mdjubayerahmad16@gmail.com>
2024-07-02 13:14:18 +0600

    Add files via upload

---
+++ Even Odd Positive and Negative.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+
+    int evenCount = 0, oddCount = 0, positiveCount = 0, negativeCount = 0;
+    int number;
+
+
+    for ( int i = 0; i <N; i++)
+    {
+        cin >> number;
+
+       if(number % 2 == 0)
+        {
+            evenCount++;
+        }
+       else
+       {
+        oddCount++;
+       }
+
+
+       if(number > 0)
+       {
+        positiveCount++;
+       }
+       else if( number < 0)
+       {
+        negativeCount++;
+       }
+
+    }
+
+    // Now, do print which has stored;
+    cout << "Even: "<< evenCount <<endl;
+    cout << "Odd: " << oddCount <<endl;
+    cout << "Positive: "<<positiveCount <<endl;
+    cout << "Negative: "<<negativeCount <<endl;
+
+
+
+return 0;
+}
